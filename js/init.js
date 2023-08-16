00fdc16bcf545b986f7c965bb5d6ebfc908b200c
robert <>
2023-08-16 10:23:41 +0800

    侧边栏目录,在小屏幕时可以展开--收起，支持横屏竖屏切换

--- js/init.js
+++ js/init.js
@@ -27,6 +27,26 @@ $(document).ready(function(){
     }, 1000);
 
     initSmallDeviceSideBar();
+
+    // 监听 orientation changes (横屏竖屏变化)
+    window.addEventListener("orientationchange", function(event) {
+        // 用的是screen.orientation API（screen.orientation.angle等于0|180、90|-90度也可以来判断横竖屏）
+        var type = screen.orientation.type;
+        
+        // 竖屏
+        if ('portrait-primary' === type) {
+            $("#sidebar").hide();
+            $('.sidebar-left-arrow').hide();
+            $('.sidebar-right-arrow').show();
+        }
+        // 横屏
+        if('landscape-primary' === type) {
+            $("#sidebar").show();
+            $('.sidebar-left-arrow').hide();
+            $('.sidebar-right-arrow').hide();
+        }
+        
+    }, false);
  });
 
  /**小屏幕，侧边目录栏初始化 */
